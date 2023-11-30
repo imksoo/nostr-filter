bbd30447530d2dd93310beda30e450ee1f3ac25e
Rif'at Ahdi R <10791791+atrifat@users.noreply.github.com>
2023-11-30 01:13:21 +0000

    fix: non string bug error handling (#21)

--- nostr-util.ts
+++ nostr-util.ts
@@ -1,11 +1,11 @@
 export const extractHashtags = (tags: string[][]): string[] => {
-  return tags.filter((tag) => tag[0] === "t").map((tag) => tag[1]);
+  return tags.filter((tag) => String(tag[0]) === "t").map((tag) => String(tag[1]));
 };
 
 export const isActivityPubUser = (tags: string[][]): boolean => {
   for (let index = 0; index < tags.length; index++) {
     const tag = tags[index];
-    if (tag.length === 3 && tag[0] === "proxy" && tag[2] === "activitypub") {
+    if (tag.length === 3 && String(tag[0]) === "proxy" && String(tag[2]) === "activitypub") {
       return true;;
     }
   }
@@ -15,7 +15,7 @@ export const isActivityPubUser = (tags: string[][]): boolean => {
 export const isRootPost = (tags: string[][]): boolean => {
   for (let index = 0; index < tags.length; index++) {
     const tag = tags[index];
-    if (tag[0] === "e") {
+    if (String(tag[0]) === "e") {
       return false;
     }
   }
@@ -25,7 +25,7 @@ export const isRootPost = (tags: string[][]): boolean => {
 export const hasContentWarning = (tags: string[][]): boolean => {
   for (let index = 0; index < tags.length; index++) {
     const tag = tags[index];
-    if (tag[0] === "content-warning") {
+    if (String(tag[0]) === "content-warning") {
       return true;
     }
   }
@@ -34,7 +34,7 @@ export const hasContentWarning = (tags: string[][]): boolean => {
 
 export const hasNsfwHashtag = (hashtags: string[]): boolean => {
   for (let index = 0; index < hashtags.length; index++) {
-    const tag = hashtags[index].toLowerCase();
+    const tag = String(hashtags[index]).toLowerCase();
     if (tag === "nsfw") {
       return true;
     }
